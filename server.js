aaae5f4d6a2e5343248cd7fe8816dacc8e4089a8
Trần Quang Thái <103127061+tranthai220302@users.noreply.github.com>
2025-03-12 23:09:20 +0700

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(bodyParser.json())
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-  origin: ['https://www.harubook.cloud'],
+  origin: ['https://hotel-manager1.onrender.com'],
   credentials: true
 }));
 const port = process.env.PORT
